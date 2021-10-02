213cec79e673ea0f0cae3e9db10f5820c4123496
renan lima <renangenito@hotmail.com>
2021-10-02 14:51:07 -0300

    Atualizando a requisição da API, incluido o primeiro CSS

--- Site-paises.js
+++ Site-paises.js
@@ -1,27 +1,73 @@
-const botao = document.querySelector("#botao")
-botao.addEventListener("click", () => {
-    var xhr = new XMLHttpRequest(); //passo 1
-    xhr.open("GET", "https://restcountries.com/v2/all") //carregando requisição //passo 2
-    
-    xhr.addEventListener("load", ()=>{
-        const resposta = xhr.responseText //Resposta da requisição porém está em Json ainda //passo3
-        
-        const paises = JSON.parse(resposta) //Convertendo Json para objeto javaScript //passo4
-		let contador = 0;
-		paises.forEach(element => {
+// const botao = document.querySelector("#botao")
+
+const main = document.querySelector("#menu-principal")
+
+
+// botao.addEventListener("click", () => {
+var xhr = new XMLHttpRequest(); //passo 1
+xhr.withCredentials = true;
+xhr.open("GET", "https://restcountries.com/v3.1/all") //carregando requisição //passo 2
+
+xhr.addEventListener("load", () => {
+	const resposta = xhr.responseText //Resposta da requisição porém está em Json ainda //passo3
+
+	const paises = JSON.parse(resposta) //Convertendo Json para objeto javaScript //passo4
+	console.log(paises)
+	// paises.forEach(pais => {
+
+
+	// 	console.log(pais.flags.png) //imagem do pais
+	// 	console.log(pais.name) // nome
+	//  	console.log(pais.population)
+	// 	console.log(pais.region)
+	// 	console.log(pais.capital) //capital
+	// 	console.log(pais.languages[0].nativeName) //idioma
+
+
+	
+		paises.forEach(pais => {
+
+			var img = new Image(); // Image constructor
+			img.src = pais.flags.png;
 			
-			console.log(element.name) // nome
-			console.log(element.flags.svg) //imagem do pais
-			console.log(element.capital) //capital
-			console.log(element.languages[0].nativeName) //idioma
-			contador++;
-			console.log(contador)
-		});
+			const principalDiv = document.createElement("div");
+			const titulo = document.createElement("h1");
+			titulo.textContent = pais.name.common;
+			const populacao = document.createElement("p")
+			populacao.textContent = "Population: " + pais.population;
+			const regiao = document.createElement("p");
+			regiao.textContent = "Region: " + pais.region;
+			const capital = document.createElement("p");
+			capital.textContent = "Capital: " + pais.capital;
 		
-        // console.log(paises);
-    })
-    
-    xhr.send()
-    
+
+			principalDiv.appendChild(img)
+			principalDiv.appendChild(titulo);
+			principalDiv.appendChild(populacao);
+			principalDiv.appendChild(regiao);
+			principalDiv.appendChild(capital);
+			
+
+
+			main.appendChild(principalDiv);
+
+			// console.log(principalDiv)
+		})
+		
+		
+	
+
+
+	
 })
+
+xhr.send()
+
+
+
+
+
+
+
+
 
---
+++ elementos.js
@@ -0,0 +1,15 @@
+/* <div>
+<img/>
+<h1>nome</h1>
+<p>população</p>
+<p>região</p>
+<p>capital</p>
+<p>idioma</p>
+</div> */
+// var img1 = new Image(); // Image constructor
+// img1.src = pais.flags.svg;
+// img1.alt = '';
+// main.appendChild(img1);
+
+
+
